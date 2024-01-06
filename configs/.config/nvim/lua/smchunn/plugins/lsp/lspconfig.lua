3179dedcf1a3cd10277ea690a4a5a1564ce07832
smchunn <smcnola1@gmail.com>
2024-01-05 18:31:36 -0600

    remove unnecessary lsp servers

--- configs/.config/nvim/lua/smchunn/plugins/lsp/lspconfig.lua
+++ configs/.config/nvim/lua/smchunn/plugins/lsp/lspconfig.lua
@@ -70,25 +70,6 @@ typescript.setup({
 	},
 })
 
--- configure css server
-lspconfig["cssls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
-
--- configure tailwindcss server
-lspconfig["tailwindcss"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
-
--- configure sqls server
-lspconfig["sqlls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	filetypes = { "sql", "mysql" },
-})
-
 -- configure python server
 lspconfig["pyright"].setup({
 	capabilities = capabilities,
@@ -101,13 +82,6 @@ lspconfig["julials"].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 	filetypes = { "julia" },
-})
-
--- configure emmet language server
-lspconfig["emmet_ls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
 })
 
 -- configure lua server (with special settings)
