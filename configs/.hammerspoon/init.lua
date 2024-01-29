be75f18678fede7678bb43ee7d23ac7134eba2a2
smchunn <smcnola1@gmail.com>
2024-01-29 08:43:23 -0500

    hammerspoon remove hotkey

--- configs/.hammerspoon/init.lua
+++ configs/.hammerspoon/init.lua
@@ -1,6 +1,6 @@
 meh = { "alt", "ctrl", "shift" }
 hs.loadSpoon("AppHotkey"):bindHotkeys({
-	["com.brave.Browser"] = { meh, "z" },
+	-- ["com.brave.Browser"] = { meh, "z" },
 	-- ["com.apple.finder"] = { meh, "q" },
 	["org.alacritty"] = { meh, "a" },
 })
