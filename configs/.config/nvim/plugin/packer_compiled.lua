715737330ad9e22d7a8ece64f768476fb0a9cc88
smchunn <smcnola1@gmail.com>
2024-09-13 18:52:22 -0400

    improve nvim/tmux/alacritty integration

--- configs/.config/nvim/plugin/packer_compiled.lua
+++ configs/.config/nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/spencer.chunn/.cache/nvim/packer_hererocks/2.1.1710088188/share/lua/5.1/?.lua;/Users/spencer.chunn/.cache/nvim/packer_hererocks/2.1.1710088188/share/lua/5.1/?/init.lua;/Users/spencer.chunn/.cache/nvim/packer_hererocks/2.1.1710088188/lib/luarocks/rocks-5.1/?.lua;/Users/spencer.chunn/.cache/nvim/packer_hererocks/2.1.1710088188/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/spencer.chunn/.cache/nvim/packer_hererocks/2.1.1710088188/lib/lua/5.1/?.so"
+local package_path_str = "/Users/spencer.chunn/.cache/nvim/packer_hererocks/2.1.1725453128/share/lua/5.1/?.lua;/Users/spencer.chunn/.cache/nvim/packer_hererocks/2.1.1725453128/share/lua/5.1/?/init.lua;/Users/spencer.chunn/.cache/nvim/packer_hererocks/2.1.1725453128/lib/luarocks/rocks-5.1/?.lua;/Users/spencer.chunn/.cache/nvim/packer_hererocks/2.1.1725453128/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/spencer.chunn/.cache/nvim/packer_hererocks/2.1.1725453128/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -190,6 +190,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/spencer.chunn/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
+  },
+  ["obsidian.nvim"] = {
+    loaded = true,
+    path = "/Users/spencer.chunn/.local/share/nvim/site/pack/packer/start/obsidian.nvim",
+    url = "https://github.com/epwalsh/obsidian.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
