1a45799dc45ec74e8d4ae05fded11cc767dfd760
smchunn <smcnola1@gmail.com>
2024-09-30 21:19:43 -0400

    nvim: fix telescope/nvim-tree integration, add minis

--- configs/.config/nvim/lua/smchunn/core/keymaps.lua
+++ configs/.config/nvim/lua/smchunn/core/keymaps.lua
@@ -1,5 +1,9 @@
 local keymap = vim.keymap -- for conciseness
 
+local function opts(desc)
+  return { desc = desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+end
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
@@ -8,90 +12,71 @@ vim.g.maplocalleader = "\\"
 ---------------------
 
 -- clear search highlights
-keymap.set("n", "<esc>", ":nohl<CR>", { desc = "Highlight Off", silent = true, noremap = true })
+keymap.set("n", "<esc>", ":nohl<CR>", opts("Highlight Off"))
 
 -- delete single character without copying into register
-keymap.set("n", "x", '"_x', { desc = "", silent = true, noremap = true })
+keymap.set("n", "x", '"_x', opts(""))
 
 -- replace single character without copying into register
-keymap.set("n", "s", '"_s', { desc = "", silent = true, noremap = true })
+keymap.set("n", "s", '"_s', opts(""))
 
 -- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Number +", silent = true, noremap = true }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Number -", silent = true, noremap = true }) -- decrement
+keymap.set("n", "<leader>+", "<C-a>", opts("Number +")) -- increment
+keymap.set("n", "<leader>-", "<C-x>", opts("Number -")) -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "v-split pane", silent = true, noremap = true }) -- split window vertically
-keymap.set("n", "<leader>sd", "<C-w>s", { desc = "h-split pane", silent = true, noremap = true }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "equate pane", silent = true, noremap = true }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", ":close<CR>", { desc = "close pane", silent = true, noremap = true }) -- close current split window
-keymap.set("n", "<leader>sk", "<C-w>k", { desc = "move up", silent = true, noremap = true }) -- move window up
-keymap.set("n", "<leader>sj", "<C-w>j", { desc = "move down", silent = true, noremap = true }) -- move window down
-keymap.set("n", "<leader>sl", "<C-w>l", { desc = "move right", silent = true, noremap = true }) -- move window right
-keymap.set("n", "<leader>sh", "<C-w>h", { desc = "move left", silent = true, noremap = true }) -- move window left
-keymap.set("n", "<leader>sK", "<C-w>+", { desc = "expand height", silent = true, noremap = true }) -- expand h
-keymap.set("n", "<leader>sJ", "<C-w>-", { desc = "shrink height", silent = true, noremap = true }) -- shrink h
-keymap.set("n", "<leader>sL", "<C-w>>", { desc = "expand width", silent = true, noremap = true }) -- expand w
-keymap.set("n", "<leader>sH", "<C-w><", { desc = "shrink width", silent = true, noremap = true }) -- shrink w
+keymap.set("n", "<leader>sv", "<C-w>v", opts("v-split pane"))     -- split window vertically
+keymap.set("n", "<leader>sd", "<C-w>s", opts("h-split pane"))     -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", opts("equate pane"))      -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>", opts("close pane"))   -- close current split window
+keymap.set("n", "<leader>sk", "<C-w>k", opts("move up"))          -- move window up
+keymap.set("n", "<leader>sj", "<C-w>j", opts("move down"))        -- move window down
+keymap.set("n", "<leader>sl", "<C-w>l", opts("move right"))       -- move window right
+keymap.set("n", "<leader>sh", "<C-w>h", opts("move left"))        -- move window left
+keymap.set("n", "<leader>sK", "<C-w>+", opts("expand height"))    -- expand h
+keymap.set("n", "<leader>sJ", "<C-w>-", opts("shrink height"))    -- shrink h
+keymap.set("n", "<leader>sL", "<C-w>>", opts("expand width"))     -- expand w
+keymap.set("n", "<leader>sH", "<C-w><", opts("shrink width"))     -- shrink w
 
-keymap.set("n", "<leader>to", ":tabnew<CR>", { desc = "new tab", silent = true, noremap = true }) -- open new tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>", { desc = "close tab", silent = true, noremap = true }) -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>", { desc = "next tab", silent = true, noremap = true }) --  go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>", { desc = "prev tab", silent = true, noremap = true }) --  go to previous tab
+keymap.set("n", "<leader>to", ":tabnew<CR>", opts("new tab"))     -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>", opts("close tab")) -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>", opts("next tab"))      --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>", opts("prev tab"))      --  go to previous tab
 
 ----------------------
 -- Plugin Keybinds
 ----------------------
 
 -- nvim-tree
-keymap.set("", "<leader>e", ":NvimTreeToggle<CR>", { desc = "", silent = true, noremap = true }) -- toggle file explorer
+keymap.set("", "<leader>e", ":NvimTreeToggle<CR>", opts("")) -- toggle file explorer
+keymap.set("n", "<C-s>", function() require("nvim-tree.api").tree.find_file({ open = true, focus = true }) end,
+  opts("expand tree to current buffer"))
+
 
 -- restart lsp server (not on youtube nvim video)
-keymap.set("n", "<leader>rs", ":LspRestart<CR>", { desc = "", silent = true, noremap = true }) -- mapping to restart lsp if necessary
+keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts("")) -- mapping to restart lsp if necessary
 
 -- inert new line above/below cursor
-keymap.set(
-	"n",
-	"gO",
-	"<Cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>",
-	{ desc = "new line above", silent = true, noremap = true }
-)
-keymap.set(
-	"n",
-	"go",
-	"<Cmd>call append(line('.'),     repeat([''], v:count1))<CR>",
-	{ desc = "new line below", silent = true, noremap = true }
-)
+keymap.set("n", "gO", "<Cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>", opts("new line above"))
+keymap.set("n", "go", "<Cmd>call append(line('.'),     repeat([''], v:count1))<CR>", opts("new line below"))
 
 -- diagnostics
-keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, { desc = "diagnostics prev", silent = true, noremap = true })
-keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, { desc = "diagnostics next", silent = true, noremap = true })
-keymap.set("n", "<leader>dd", vim.diagnostic.open_float, { desc = "diagnostics float", silent = true, noremap = true })
-keymap.set("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "diagnostics list", silent = true, noremap = true })
+keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, opts("diagnostics prev"))
+keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, opts("diagnostics next"))
+keymap.set("n", "<leader>dd", vim.diagnostic.open_float, opts("diagnostics float"))
+keymap.set("n", "<leader>ds", vim.diagnostic.setloclist, opts("diagnostics list"))
 
-keymap.set("n", "<leader>on", ":ObsidianNew<CR>", { desc = "Obsidian New File", silent = true, noremap = true })
-keymap.set(
-	"n",
-	"<leader>ot",
-	":ObsidianNewFromTemplate<CR>",
-	{ desc = "Obsidian New File From Template", silent = true, noremap = true }
-)
-keymap.set(
-	"n",
-	"<leader>oT",
-	":ObsidianTemplate<CR>",
-	{ desc = "Obsidian New Template", silent = true, noremap = true }
-)
-keymap.set(
-	"n",
-	"<leader>ow",
-	":ObsidianWorkspace<CR>",
-	{ desc = "Obsidian Switch Workspace", silent = true, noremap = true }
-)
-keymap.set(
-	"n",
-	"<leader>oo",
-	":ObsidianQuickSwitch<CR>",
-	{ desc = "Obsidian Quick Switch", silent = true, noremap = true }
-)
-keymap.set("n", "<leader>os", ":ObsidianSearch<CR>", { desc = "Obsidian Search", silent = true, noremap = true })
+-- obsidian
+keymap.set("n", "<leader>on", ":ObsidianNew<CR>", opts("Obsidian New File"))
+keymap.set("n", "<leader>ot", ":ObsidianNewFromTemplate<CR>", opts("Obsidian New File From Template"))
+keymap.set("n", "<leader>oT", ":ObsidianTemplate<CR>", opts("Obsidian New Template"))
+keymap.set("n", "<leader>ow", ":ObsidianWorkspace<CR>", opts("Obsidian Switch Workspace"))
+keymap.set("n", "<leader>oo", ":ObsidianQuickSwitch<CR>", opts("Obsidian Quick Switch"))
+keymap.set("n", "<leader>os", ":ObsidianSearch<CR>", opts("Obsidian Search"))
+-- dashboard
+keymap.set("n", "<leader>db", ":Dashboard<CR>", opts("dashboard"))
+
+-- keymap.set("", "<C-r>", function()
+--   local current_buf = vim.api.nvim_get_current_buf()
+--   print("current buffer type: " .. vim.bo[current_buf].filetype)
+-- end)
--- configs/.config/nvim/lua/smchunn/plugins/telescope.lua
+++ configs/.config/nvim/lua/smchunn/plugins/telescope.lua
@@ -1,5 +1,122 @@
 local M = { actions = {}, previewers = {} }
 
+function M.actions.git_apply_stash_file(stash_id)
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+  local utils = require("smchunn.core.utils")
+
+  return function(prompt_bufnr)
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    local selection = picker:get_multi_selection()
+    if #selection == 0 then
+      selection = { picker:get_selection() }
+    end
+
+    local files = {}
+    for _, sel in ipairs(selection) do
+      table.insert(files, sel[1])
+    end
+    local git_command = {
+      "sh",
+      "-c",
+      "git --no-pager diff HEAD.." .. " | git apply -",
+    }
+
+    local _, ret, stderr = utils.get_os_command_output(git_command)
+    if ret == 0 then
+      print("Applied stash files from " .. stash_id)
+    else
+      print("Error applying stash " .. vim.inspect(stderr))
+    end
+    actions.close(prompt_bufnr)
+  end
+end
+
+function M.actions.show_git_stash_files()
+  local actions = require("telescope.actions")
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local action_state = require("telescope.actions.state")
+  local telescope_config = require("telescope.config").values
+
+  local selection = action_state.get_selected_entry()
+  if selection == nil or selection.value == nil or selection.value == "" then
+    return
+  end
+
+  local stash_id = selection.value
+
+  local opts = {}
+  local p = pickers.new(opts, {
+    prompt_title = "Files in " .. stash_id,
+    finder = finders.new_oneshot_job({
+      "git",
+      "--no-pager",
+      "stash",
+      "show",
+      stash_id,
+      "--name-only",
+    }, opts),
+    previewer = M.previewers.git_stash_file(stash_id, opts),
+    sorter = telescope_config.file_sorter(opts),
+    attach_mappings = function()
+      actions.select_default:replace(M.actions.git_apply_stash_file(stash_id))
+      return true
+    end,
+  })
+  p:find()
+end
+
+function M.previewers.git_stash_file(stash_id, opts)
+  local previewers = require("telescope.previewers")
+  local putils = require("telescope.previewers.utils")
+
+  return previewers.new_buffer_previewer({
+    title = "Stash file preview",
+    get_buffer_by_name = function(_, entry)
+      return entry.value
+    end,
+    define_preview = function(self, entry, _)
+      local cmd = { "git", "--no-pager", "diff", stash_id, "--", entry.value }
+      putils.job_maker(cmd, self.state.bufnr, {
+        value = entry.value,
+        bufname = self.state.bufname,
+        cwd = opts.cwd,
+        callback = function(bufnr)
+          if vim.api.nvim_buf_is_valid(bufnr) then
+            putils.regex_highlighter(bufnr, "diff")
+          end
+        end,
+      })
+    end,
+  })
+end
+
+function M.actions.open_and_resume(prompt_bufnr)
+  require("telescope.actions").select_default(prompt_bufnr)
+  require("telescope.builtin").resume()
+end
+
+function M.actions.open_in_nvim_tree(prompt_bufnr)
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+  local nvim_tree_api = require("nvim-tree.api")
+
+  local selection = action_state.get_selected_entry()
+  if not selection then
+    print("No selection made")
+    return
+  end
+
+  local selection_path = selection.value
+
+  actions.close(prompt_bufnr)
+
+  print("open...")
+  nvim_tree_api.tree.open()
+  nvim_tree_api.tree.find_file({ buf = selection_path, open = true, focus = true })
+end
+
 return {
   "nvim-telescope/telescope.nvim",
   branch = "0.1.x",
@@ -52,13 +169,7 @@ return {
             ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
             ["<C-s>"] = M.actions.open_in_nvim_tree,
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            ["<esc>"] = actions.close,
-          },
-          n = {
-            ["<C-h>"] = actions.select_horizontal,
-            ["<C-o>"] = M.actions.open_and_resume,
-            ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
-            ["<C-s>"] = M.actions.open_in_nvim_tree,
+            -- ["<esc>"] = actions.close,
           },
         },
       },
@@ -109,121 +220,6 @@ return {
       { desc = "Telescope Git Status", silent = true, noremap = true })
     vim.keymap.set("n", "z=", ":Telescope spell_suggest<CR>",
       { desc = "Telescope Spell Suggest", silent = true, noremap = true })
-
-    function M.actions.git_apply_stash_file(stash_id)
-      local actions = require("telescope.actions")
-      local action_state = require("telescope.actions.state")
-
-      return function(prompt_bufnr)
-        local picker = action_state.get_current_picker(prompt_bufnr)
-        local selection = picker:get_multi_selection()
-        if #selection == 0 then
-          selection = { picker:get_selection() }
-        end
-
-        local files = {}
-        for _, sel in ipairs(selection) do
-          table.insert(files, sel[1])
-        end
-        local git_command = {
-          "sh",
-          "-c",
-          "git --no-pager diff HEAD.." .. " | git apply -",
-        }
-
-        local _, ret, stderr = utils.get_os_command_output(git_command)
-        if ret == 0 then
-          print("Applied stash files from " .. stash_id)
-        else
-          print("Error applying stash " .. vim.inspect(stderr))
-        end
-        actions.close(prompt_bufnr)
-      end
-    end
-
-    function M.actions.show_git_stash_files()
-      local actions = require("telescope.actions")
-      local pickers = require("telescope.pickers")
-      local finders = require("telescope.finders")
-      local action_state = require("telescope.actions.state")
-      local telescope_config = require("telescope.config").values
-
-      local selection = action_state.get_selected_entry()
-      if selection == nil or selection.value == nil or selection.value == "" then
-        return
-      end
-
-      local stash_id = selection.value
-
-      local opts = {}
-      local p = pickers.new(opts, {
-        prompt_title = "Files in " .. stash_id,
-        finder = finders.new_oneshot_job({
-          "git",
-          "--no-pager",
-          "stash",
-          "show",
-          stash_id,
-          "--name-only",
-        }, opts),
-        previewer = M.previewers.git_stash_file(stash_id, opts),
-        sorter = telescope_config.file_sorter(opts),
-        attach_mappings = function()
-          actions.select_default:replace(M.actions.git_apply_stash_file(stash_id))
-          return true
-        end,
-      })
-      p:find()
-    end
-
-    function M.previewers.git_stash_file(stash_id, opts)
-      local putils = require("telescope.previewers.utils")
-      local previewers = require("telescope.previewers")
-
-      return previewers.new_buffer_previewer({
-        title = "Stash file preview",
-        get_buffer_by_name = function(_, entry)
-          return entry.value
-        end,
-        define_preview = function(self, entry, _)
-          local cmd = { "git", "--no-pager", "diff", stash_id, "--", entry.value }
-          putils.job_maker(cmd, self.state.bufnr, {
-            value = entry.value,
-            bufname = self.state.bufname,
-            cwd = opts.cwd,
-            callback = function(bufnr)
-              if vim.api.nvim_buf_is_valid(bufnr) then
-                putils.regex_highlighter(bufnr, "diff")
-              end
-            end,
-          })
-        end,
-      })
-    end
-
-    function M.actions.open_and_resume(prompt_bufnr)
-      require("telescope.actions").select_default(prompt_bufnr)
-      require("telescope.builtin").resume()
-    end
-
-    function M.actions.open_in_nvim_tree(prompt_bufnr)
-      local action_state = require("telescope.actions.state")
-      local actions = require("telescope.actions")
-      local nvim_tree_api = require("nvim-tree.api")
-
-      local selection = action_state.get_selected_entry()
-      if not selection then
-        print("No selection made")
-        return
-      end
-
-      local selection_path = selection.value
-
-      actions.close(prompt_bufnr)
-
-      nvim_tree_api.tree.open()
-      nvim_tree_api.tree.find_file({ buf = selection_path, open = true })
-    end
 
     telescope.load_extension("fzf")
   end,
