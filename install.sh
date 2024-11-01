6ac16b2acd57b294d37ce052e489fbc5c1272840
smchunn <smcnola1@gmail.com>
2024-11-01 10:42:34 -0400

    change to bash script

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
-#!/bin/zsh
-# Define a function which rename a `target` file to `target.backup` if the file
-# exists and if it's a 'real' file, ie not a symlink
+#!/bin/bash
+
+# Define a function which renames a `target` file to `target.bak` if the file
+# exists and if it's a 'real' file, i.e., not a symlink
 backup() {
   target=$1
+  date=$(date '+%Y-%m-%d_%H:%M:%S')
   if [ -e "$target" ]; then
     if [ ! -L "$target" ]; then
-      mv "$target" "$target.bak"
-      echo "> $target backed up to $target.bak"
+      mv "$target" "$target.$date.bak"
+      echo "> $target backed up to $target.$date.bak"
     fi
   fi
 }
@@ -15,79 +17,127 @@ symlink() {
   file=$1
   link=$2
   echo "> Symlinking $link to $file"
-  ln -nsf $file $link
+  ln -nsf "$file" "$link"
 }
 
-zparseopts -D -F -K   \
-  {a,-all}=opts_all   \
-  {f,-file}=opts_file \
-  {d,-dir}=opts_dir   \
-  {z,-zsh}=opts_zsh   \
-  {t,-tpm}=opts_tpm
+# Initialize option variables
+opts_update=false
+opts_force=false
 
+# Parse options using getopts
+while getopts "uf" opt; do
+  case $opt in
+    u) opts_update=true ;;
+    f) opts_force=true ;;
+    *) echo "Invalid option"; exit 1 ;;
+  esac
+done
 
-# For all files `$name` in the present folder except `*.sh`, `README.md`, `settings.json`,
+echo "Zsh:"
+# Detect OS and install/update Zsh
+if [[ "$(uname)" == "Darwin" ]]; then
+  echo "::: Detected macOS. Checking for Homebrew Zsh..."
+  if ! brew list zsh &>/dev/null; then
+    echo "::: Zsh not found. Installing Zsh via Homebrew..."
+    brew install zsh
+  elif [ "$opts_update" == true ]; then
+    echo "::: Updating Zsh via Homebrew..."
+    brew upgrade zsh
+  else
+    echo "::: Zsh is already installed. Skipping installation."
+  fi
+elif [[ "$(uname -s)" == "Linux" ]]; then
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    if [[ "$ID" == "ubuntu" ]]; then
+      echo "::: Detected Ubuntu. Checking for apt Zsh..."
+      if ! dpkg -l | grep -q zsh; then
+        echo "::: Zsh not found. Installing Zsh via apt..."
+        sudo apt update && sudo apt install -y zsh
+      elif [ "$opts_update" == true ]; then
+        echo "::: Updating Zsh via apt..."
+        sudo apt update && sudo apt upgrade -y zsh
+      else
+        echo "::: Zsh is already installed. Skipping installation."
+      fi
+    fi
+  fi
+fi
+
+# For all files in the present folder except `*.sh`, `README.md`, `settings.json`,
 # and `config`, backup the target file located at `~/.$name` and symlink `$name` to `~/.$name`
-if (($#opts_all + $#opts_file > 0)); then
-  echo "Linking Files..."
-  for fp in `find ~+/configs -not -name ".DS_Store" -mindepth 1 -maxdepth 1`; do
-    name=${fp##*/}
-    if [ ! -d "$name" ]; then
-      target="$HOME/$name"
-      backup $target
-      symlink "$fp" $target
-    fi
-  done
-fi
-
-if (($#opts_all + $#opts_dir > 0)); then
-  echo "Linking Dirs..."
-  for fp in `find ~+/configs -mindepth 1 -maxdepth 1`; do
-    name=${fp##*/}
-    if [ -d "$name" ]; then
-      target="$HOME/$name"
-      backup $target
-      symlink "$fp" $target
-    fi
-  done
-fi
-
-
-if (($#opts_all + $#opts_zsh > 0)); then
-  echo "Installing OMZ..."
-  if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
+echo "Linking Files..."
+for fp in $(find ~/configs -not -name ".DS_Store" -mindepth 1 -maxdepth 1); do
+  name=${fp##*/}
+  # Check if name is not a directory and either opts_force is true or the target is not a symlink
+  if [[ ! -d "$name" && ( "$opts_force" == true || ! -h "$HOME/$name" ) ]]; then
+    target="$HOME/$name"
+    backup "$target"
+    symlink "$fp" "$target"
   fi
-  ZSH_THEMES_DIR="$HOME/.oh-my-zsh/custom/themes"
-  if [ ! -d "$ZSH_THEMES_DIR/typewritten" ]; then
-    git clone https://github.com/reobin/typewritten.git $ZSH_THEMES_DIR/typewritten
+done
+
+echo "Linking Dirs..."
+for fp in $(find ~/configs -mindepth 1 -maxdepth 1); do
+  name=${fp##*/}
+  # Check if name is a directory and either opts_force is true or the target is not a symlink
+  if [[ -d "$name" && ( "$opts_force" == true || ! -h "$HOME/$name" ) ]]; then
+    target="$HOME/$name"
+    backup "$target"
+    symlink "$fp" "$target"
   fi
-  ln -nsf "$ZSH_THEMES_DIR/typewritten/typewritten.zsh-theme" "$ZSH_THEMES_DIR/typewritten.zsh-theme"
-  ln -nsf "$ZSH_THEMES_DIR/typewritten/async.zsh" "/$ZSH_THEMES_DIR/async"
+done
+
+echo "OMZ:"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
+elif [ "$opts_update" == true ]; then
+  (omz update)
 fi
 
-# Install zsh-syntax-highlighting plugin
-if (($#opts_all + $#opts_zsh > 0)); then
-  CURRENT_DIR=`pwd`
-  ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
-  mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
-  if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
-    echo "::: Installing zsh plugin 'zsh-syntax-highlighting'..."
-    git clone https://github.com/zsh-users/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting
-  fi
-
-  cd "$CURRENT_DIR"
+echo "::: Installing theme: 'typewritten'..."
+ZSH_THEMES_DIR="$HOME/.oh-my-zsh/custom/themes"
+if [[ ! -d "$ZSH_THEMES_DIR/typewritten" ]]; then
+  git clone https://github.com/reobin/typewritten.git "$ZSH_THEMES_DIR/typewritten"
+elif [ "$opts_update" == true ]; then
+  echo "::: 'typewritten' updating..."
+  (cd "$ZSH_THEMES_DIR/typewritten" && git pull)
+else
+  echo "::: 'typewritten' directory exists, skipping..."
 fi
 
-
-# Install tpm
-if (($#opts_all + $#opts_tpm > 0)); then
-  echo "Installing TPM..."
-  if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-    echo "::: Installing tmux plugin manager..."
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-  fi
+ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
+if [ ! -d "$ZSH_PLUGINS_DIR" ]; then
+  mkdir -p "$ZSH_PLUGINS_DIR"
 fi
 
+if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
+  echo "::: Installing zsh plugin 'zsh-syntax-highlighting'..."
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting
+elif [ "$opts_update" == true ]; then
+  echo "::: Updating zsh plugin 'zsh-syntax-highlighting'..."
+  (cd "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" && git pull)
+else
+  echo "::: 'zsh-syntax-highlighting' directory exists, skipping..."
+fi
 
+if [ ! -d "$ZSH_PLUGINS_DIR/zsh-autosuggestions" ]; then
+  echo "::: Installing zsh plugin 'zsh-autosuggestions'..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions
+elif [ "$opts_update" == true ]; then
+  echo "::: Updating zsh plugin 'zsh-autosuggestions'..."
+  (cd "$ZSH_PLUGINS_DIR/zsh-autosuggestions" && git pull)
+else
+  echo "::: 'zsh-autosuggestions' directory exists, skipping..."
+fi
+
+echo "TPM:"
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  echo "::: Installing tmux plugin manager..."
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+elif [ "$opts_update" == true ]; then
+  echo "::: Updating tmux plugin manager..."
+  (cd "$HOME/.tmux/plugins/tpm" && git pull)
+else
+  echo "::: 'TPM' directory exists, skipping..."
+fi
