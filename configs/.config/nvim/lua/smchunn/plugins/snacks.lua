08fe365ca4b7f85010659719e5e04088f4cf7320
smchunn <smchunn@icloud.com>
2024-12-18 09:49:01 -0500

    sync nvim config

---
+++ configs/.config/nvim/lua/smchunn/plugins/snacks.lua
@@ -0,0 +1,43 @@
+return {
+  "snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    indent = { enabled = true },
+    input = { enabled = true },
+    notifier = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    words = { enabled = true },
+    dashboard = {
+      preset = {
+        header = [[]],
+        -- stylua: ignore
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      sections = {
+        {
+          section = "terminal",
+          cmd = "colorsquares",
+          height = 5,
+          padding = 2,
+        },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+      },
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Notification History" },
+    { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+  },
+}
