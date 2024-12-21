54502a68ee54a9e27c7e0f1faf0fe67ceaf596a8
smchunn <smchunn@icloud.com>
2024-12-21 17:09:39 -0600

    telescope max file size for preview

--- configs/.config/nvim/lua/smchunn/plugins/telescope.lua
+++ configs/.config/nvim/lua/smchunn/plugins/telescope.lua
@@ -76,6 +76,13 @@ return {
       extensions = {
         egrepify = {},
       },
+      preview = {
+        filesize_hook = function(filepath, bufnr, opts)
+          local max_bytes = 10000
+          local cmd = { "head", "-c", max_bytes, filepath }
+          require("telescope.previewers.utils").job_maker(cmd, bufnr, opts)
+        end,
+      },
     })
 
     -- Key mappings
