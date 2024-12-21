907e33f75e9abea6f34f01d52ddfd3f692da3f9c
smchunn <smchunn@icloud.com>
2024-12-21 17:09:10 -0600

    add snacks plugin

--- configs/.config/nvim/lua/smchunn/plugins/snacks.lua
+++ configs/.config/nvim/lua/smchunn/plugins/snacks.lua
@@ -4,6 +4,7 @@ return {
   lazy = false,
   opts = {
     indent = { enabled = true },
+    dim = { enabled = true },
     input = { enabled = true },
     notifier = { enabled = true },
     scope = { enabled = true },
@@ -23,6 +24,17 @@ return {
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
       },
+      formats = {
+        icon = function(item)
+          return { { item.icon, hl = "MoonflyBlue" } }
+        end,
+        desc = function(item)
+          return { { item.desc, hl = "MoonflyBlue" } }
+        end,
+        key = function(item)
+          return { { item.key, hl = "MoonflyBlue" } }
+        end,
+      },
       sections = {
         {
           section = "terminal",
@@ -39,5 +51,39 @@ return {
   keys = {
     { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Notification History" },
     { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+    { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+    { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+    { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+    { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+    { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
+    { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+    { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
   },
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      callback = function()
+        -- Setup some globals for debugging (lazy-loaded)
+        _G.dd = function(...)
+          Snacks.debug.inspect(...)
+        end
+        _G.bt = function()
+          Snacks.debug.backtrace()
+        end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+        -- Create some toggle mappings
+        Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+        Snacks.toggle.diagnostics():map("<leader>ud")
+        Snacks.toggle.treesitter():map("<leader>uT")
+        Snacks.toggle.inlay_hints():map("<leader>uh")
+        Snacks.toggle.indent():map("<leader>ug")
+        Snacks.toggle.scroll():map("<leader>uj")
+        Snacks.toggle.dim():map("<leader>uD")
+      end,
+    })
+  end,
 }
